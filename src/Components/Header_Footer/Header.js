e927af24516a3505f10481342dfb62bf38b2dda3
Chris Bistline <chris.bistline@gmail.com>
2019-07-11 14:54:00 -0500

    creating basic layout components

---
+++ src/Components/Header_Footer/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+
+import { Link } from 'react-router-dom';
+
+class Header extends Component {
+    render() {
+        return (
+            <AppBar
+                position="fixed"
+            >
+
+            </AppBar>
+        );
+    }
+}
+
+export default Header;
---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from './HOC/Layout'
+
+const Routes = (props) => {
+  return(
+    <div>
+      <Layout>
+        second line
+      </Layout>
+    </div>
+  )
+}
+
+
+export default Routes;
